07b4a2e24a5f9231698a5900538ab22140d34e95
Sanjay106 <Sanjay.punch@gmail.com>
2017-08-02 01:09:41 +0530

    Delete faculty1.java

--- polymorphism/multilevelInheritance/faculty1.java
+++
@@ -1,9 +0,0 @@
-package oops.inheritance.multilevelInheritance;
-
-public class faculty1 {
-public void getfacultydetails1(String name,int age ) 
-{
-	System.out.println("the name of the student is "+name);
-	System.out.println("the id of the student is "+age);
-}
-}
