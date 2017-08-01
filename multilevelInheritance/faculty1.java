df25b68a2d0800fd8892cc0eb1b28b569cc03a72
Sanjay106 <Sanjay.punch@gmail.com>
2017-08-02 01:04:41 +0530

    Add files via upload

---
+++ multilevelInheritance/faculty1.java
@@ -0,0 +1,9 @@
+package oops.inheritance.multilevelInheritance;
+
+public class faculty1 {
+public void getfacultydetails1(String name,int age ) 
+{
+	System.out.println("the name of the student is "+name);
+	System.out.println("the id of the student is "+age);
+}
+}
